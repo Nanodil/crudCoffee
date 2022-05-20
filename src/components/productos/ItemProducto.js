96159dbf08cf74eea4223aba08d2ed6f94636c7a
nano <marianodilella86@gmail.com>
2022-05-20 20:16:15 -0300

    se agrego la logica del boton borrar

--- src/components/productos/ItemProducto.js
+++ src/components/productos/ItemProducto.js
@@ -1,7 +1,48 @@
 import React from "react";
 import { ListGroup, Button } from "react-bootstrap";
+import Swal from 'sweetalert2';
+
 
 const ItemProducto = (props) => {
+const URL = process.env.REACT_APP_API_URL+'/'+props.producto.id
+
+  const eliminarProducto = ()=>{
+    //console.log(URL);
+    Swal.fire({
+      title: 'Esta seguro de eliminar el producto?',
+      text: "No puedes recuperar un producto una vez eiminado!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Borrar',
+      cancelButtonText: 'Cancelar',
+    }).then(async(result) => {
+      if (result.isConfirmed) {
+        //aqui borro el producto
+        try{
+const respuesta = await fetch(URL,{method: "DELETE",
+headers: {
+  "Content-Type": "application/json",
+}
+        });
+console.log(respuesta)
+
+
+        }catch(error){
+        console.log(error);
+      }
+        //cartel informativo para el usuario
+        Swal.fire(
+          'Producto eliminado',
+          'El producto fue correctamente borrado',
+          'success'
+        )
+      }
+    })
+  }
+
+
   return (
     <ListGroup.Item className="d-flex justify-content-between">
       <p>
@@ -10,7 +51,7 @@ const ItemProducto = (props) => {
       </p>
       <div>
         <Button variant="warning">Editar</Button>
-        <Button variant="danger">Borrar</Button>
+        <Button variant="danger" onClick={()=> eliminarProducto()}>Borrar</Button>
       </div>
     </ListGroup.Item>
   );
